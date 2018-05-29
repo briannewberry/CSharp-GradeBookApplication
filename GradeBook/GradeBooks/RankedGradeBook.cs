94936bb664c8ae8279f4de08500205d5c32e27d8
Brian Newberry <newberrybrianc@gmail.com>
2018-05-29 15:24:25 -0500

    7-11

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,44 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var thresh = (int)Math.Ceiling(Students.Count * 0.2);
+            int numBelow = 0;
+            
+            foreach (Student student in Students)
+            {
+                if(averageGrade > student.AverageGrade)
+                {
+                    numBelow++;
+                }
+            }
+
+            if(numBelow >= (Students.Count - thresh))
+            {
+                return 'A';
+            } else if ((numBelow >= Students.Count - (thresh*2))) {
+                return 'B';
+            }
+            else if ((numBelow >= Students.Count - (thresh * 3)))
+            {
+                return 'C';
+            }
+            else if ((numBelow >= Students.Count - (thresh * 4)))
+            {
+                return 'D';
+            }
+
+
+
+
+            return 'F';
+        }
     }
 }
